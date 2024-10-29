d9ed35a21acfd33c72e3b00d243484c074754566
Daniel Margolis <dan@af0.net>
2024-10-29 18:20:57 +0000

    Add option to export in Instapaper format.

--- omnivore_backup/__init__.py
+++ omnivore_backup/__init__.py
@@ -1,5 +1,7 @@
 import argparse
 import os.path
+import csv
+import datetime
 import json
 
 from omnivoreql import OmnivoreQL
@@ -9,7 +11,11 @@ from retry import retry
 def main():
     parser = argparse.ArgumentParser(description="Backup Omnivore")
     parser.add_argument(
-        "--backup", type=str, default="./export", help="Backup directory"
+        "--backup", type=str, default="", help="Backup directory"
+    )
+    parser.add_argument(
+        # Export in Instapaper format: URL,Title,Selection,Folder,Timestamp
+        "--csv", type=str, default="", help="CSV to export URLs to"
     )
     parser.add_argument("--api_key", type=str, help="API Key")
     args = parser.parse_args()
@@ -20,9 +26,10 @@ def main():
     omnivoreql_client = OmnivoreQL(args.api_key)
 
     @retry(delay=1, backoff=2)
-    def get_articles(cursor=None):
+    def get_articles(cursor=0):
         print(f"Fetching page {cursor if cursor else 0}...")
-        page = omnivoreql_client.get_articles(cursor=cursor)["search"]
+        page = omnivoreql_client.get_articles(after=cursor)["search"]
+        #page = omnivoreql_client.get_articles()["search"]
         if not page["pageInfo"]["hasNextPage"]:
             return page["edges"]
         return page["edges"] + get_articles(cursor=page["pageInfo"]["endCursor"])
@@ -35,22 +42,40 @@ def main():
 
     pages = get_articles()
 
+    writer = None
+    csv_writer = None
+    if args.csv:
+        writer = open(args.csv, "w", newline="")
+        csv_writer = csv.writer(writer, delimiter=",")
+        csv_writer.writerow(["URL", "Title", "Selection", "Folder", "Timestamp"])
     for article in pages:
         print(f'Saving {article["node"]["slug"]}...')
-        path = os.path.join(args.backup, article["node"]["slug"])
-        meta = article["node"]
-        if os.path.exists(path):
-            # Update the metadata only:
+        if args.backup:
+            path = os.path.join(args.backup, article["node"]["slug"])
+            meta = article["node"]
+            if os.path.exists(path):
+                # Update the metadata only:
+                with open(os.path.join(path, "meta.json"), "w") as metadata:
+                    json.dump(meta, metadata)
+                continue
+            # Get content:
+            os.mkdir(path)
+            content = get_article(article["node"]["slug"])["content"]
             with open(os.path.join(path, "meta.json"), "w") as metadata:
                 json.dump(meta, metadata)
-            continue
-        # Get content:
-        os.mkdir(path)
-        content = get_article(article["node"]["slug"])["content"]
-        with open(os.path.join(path, "meta.json"), "w") as metadata:
-            json.dump(meta, metadata)
-        with open(os.path.join(path, "content.md"), "w") as markdown:
-            markdown.write(content)
+            with open(os.path.join(path, "content.md"), "w") as markdown:
+                markdown.write(content)
+        if args.csv:
+            csv_writer.writerow([
+                article["node"]["url"], 
+                article["node"]["title"],
+                "",
+                article["node"]["folder"],
+                int(datetime.datetime.fromisoformat(article["node"]["createdAt"]).timestamp())
+                ,
+            ])
+
+
 
 
 main()
